f85eebf640b865dc013cb59c93373684697fb14b
Tony Wang <wwwjfy@gmail.com>
2013-08-30 17:35:23 +0800

    [fish] adb completion
    don't complete it when operation specified already

--- fish/completions/adb.fish
+++ fish/completions/adb.fish
@@ -1,1 +1,9 @@
-complete -f -c adb -a 'devices connect disconnect push pull sync shell shell emu logcat forward jdwp install uninstall bugreport backup restore help version wait-for-device start-server kill-server get-state get-serialno get-devpath status-window remount reboot reboot-bootloader root usb tcpip ppp'
+function __adb_need_arg
+    set cmd (commandline -opc)
+    if [ (count $cmd) -gt 1 ]
+        return 1
+    end
+    return 0
+end
+
+complete -f -c adb -n '__adb_need_arg' -a 'devices connect disconnect push pull sync shell shell emu logcat forward jdwp install uninstall bugreport backup restore help version wait-for-device start-server kill-server get-state get-serialno get-devpath status-window remount reboot reboot-bootloader root usb tcpip ppp'
