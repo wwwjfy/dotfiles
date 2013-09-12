710016985f3379bf921fc865a88b649b87f999d6
Tony Wang <wwwjfy@gmail.com>
2013-09-12 21:09:57 +0800

    [fish] prevent multiple get_github_contribution

--- fish/get_github_contribution.fish
+++ fish/get_github_contribution.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
 
+touch $argv[2]
 curl -s https://github.com/users/$argv[1]/contributions_calendar_data ^/dev/null | python -c 'import json; print(json.loads(raw_input())[-1][1])' > $argv[2].tmp
 mv $argv[2].tmp $argv[2]
