e714c854bb0a0f20c2eaac0337cc90eefdc2907f
Tony Wang <wwwjfy@gmail.com>
2013-09-17 08:14:08 +0800

    [fish] in case github_contrib_file doesn't exist

--- fish/config.fish
+++ fish/config.fish
@@ -143,7 +143,7 @@ end
 # }}}
 
 if test (uname) = "Darwin"
-    if test (/usr/bin/stat -f "%m" $github_contrib_file) -lt (expr (/bin/date "+%s") - 3600)
+    if test ! -e $github_contrib_file; or test (/usr/bin/stat -f "%m" $github_contrib_file) -lt (expr (/bin/date "+%s") - 3600)
         bash ~/dotfiles/get_github_contribution.sh wwwjfy $github_contrib_file &
     end
 end
