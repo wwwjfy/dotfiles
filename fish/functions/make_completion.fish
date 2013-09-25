d39f98aa2ff0deed94725ee345ba172423b112dc
Tony Wang <wwwjfy@gmail.com>
2013-09-25 21:54:09 +0800

    [fish] fix make_completion for "\"
    So the problem is "\" in the input will be parsed as plain "\",
    instead of an escape of the following character. e.g. a directory
    named "a b" will be "a\ b" on first tab, and then "a\\ b" on second
    tab.
    
    There still is a problem that if " is used to avoid using "\", the quote
    itself will be escaped, returning funny result. Anyway, it's not
    conflicting with my habbits, and I don't have a solution now after
    spending some time, so I'll just leave it now.

--- fish/functions/make_completion.fish
+++ fish/functions/make_completion.fish
@@ -1,7 +1,7 @@
 function make_completion --argument alias command
     eval "function $alias; $command \$argv; end"
     complete -c $alias -xa "(
-        set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' );
-        complete -C\"$command \$cmd\";
+        set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' -e 's/\\\\\\\\\\\\(.\\\\)/\\\\1/' )
+        complete -C\"$command \$cmd\"
     )"
 end
