000f4d979ff26090234c0670b3fde70e183753a2
Tony Wang <wwwjfy@gmail.com>
2014-08-29 13:15:44 +0800

    remove zsh

--- setup.sh
+++ setup.sh
@@ -62,9 +62,6 @@ if [[ ! -a ".vimrc" ]]; then
 fi
 
 # zsh
-if [[ ! -a ".oh-my-zsh" ]]; then
-    git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
-fi
 link_file zshrc
 link_file tmux.conf
 link_file gitconfig
