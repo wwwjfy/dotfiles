f06e00926322466ef236d73b7abb0e58326c44cc
Tony Wang <tony@initialcommit.net>
2025-03-21 14:51:00 +0800

    [hammerspoon] fix maximize window for firefox

--- hammerspoon/layout.lua
+++ hammerspoon/layout.lua
@@ -1,7 +1,34 @@
+local function axHotfix(win)
+    if not win then win = hs.window.frontmostWindow() end
+
+    local axApp = hs.axuielement.applicationElement(win:application())
+    local wasEnhanced = axApp.AXEnhancedUserInterface
+    axApp.AXEnhancedUserInterface = false
+
+    return function()
+        hs.timer.doAfter(hs.window.animationDuration * 2, function()
+            axApp.AXEnhancedUserInterface = wasEnhanced
+        end)
+    end
+end
+
+local function withAxHotfix(fn, position)
+    if not position then position = 1 end
+    return function(...)
+        local revert = axHotfix(select(position, ...))
+        fn(...)
+        revert()
+    end
+end
+
+local windowMT = hs.getObjectMetatable("hs.window")
+windowMT._setFrameInScreenBounds = windowMT._setFrameInScreenBounds or windowMT.setFrameInScreenBounds -- Keep the original, if needed
+windowMT.setFrameInScreenBounds = withAxHotfix(windowMT.setFrameInScreenBounds)
+
 hs.hotkey.bind({"ctrl"}, ",", function()
     local win = hs.window.frontmostWindow()
     local screen = win:screen()
-    win:setFrame(screen:frame())
+    win:setFrameInScreenBounds(screen:frame())
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, ";", function()
